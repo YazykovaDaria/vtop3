c7a09a0cc09ad81a4d409744fe70125562294e9a
YazykovaDaria <yazykovadasva@gmail.com>
2024-05-18 22:55:34 +0300

    feat: responsive slider

--- src/js/helpers/helpers.js
+++ src/js/helpers/helpers.js
@@ -6,6 +6,15 @@ export const getChild = (parentEl, selector) =>
 export const getAllChild = (parentEl, selector) =>
   parentEl ? parentEl.querySelectorAll(selector) : null;
 
+export const getCard = (pet) => {
+  const template = `<article class="card js-open-popup" data-id="${pet.id}">
+    <img class="card__img" src="${pet.img}" alt='pet'/>
+    <p class="title card__title">${pet.name}</p>
+    <button class="btn card__btn" type='button'>Learn more</button>
+    </article>`;
+  return template;
+};
+
 const blockPageScroll = (isBlock = true) => {
   const htmlElement = document.documentElement;
   if (isBlock) {
--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,20 +1,66 @@
-import { getChild, getAllChild } from "../helpers/helpers";
-// import { getAllPets } from "../store/store";
+import { getChild, getCard, getAllChild } from '../helpers/helpers';
+import { getAllPets } from '../store/store';
 
 const SLIDER_SELECTOR = '.js-slider';
 const SLIDER_WRAPPER_SELECTOR = '.js-slider-wrapper';
-const SLIDE_SELECTOR = '.js-slide';
 const PREV_BTN_SELECTOR = '.js-slide-prev';
 const NEXT_BTN_SELECTOR = '.js-slide-next';
 
 const sliderEl = document.querySelector(SLIDER_SELECTOR);
 
 const sliderWrapper = getChild(sliderEl, SLIDER_WRAPPER_SELECTOR);
-const slides = getAllChild(sliderEl, SLIDE_SELECTOR);
-const prevButton = getChild(sliderEl, PREV_BTN_SELECTOR);
-const nextButton = getChild(sliderEl, NEXT_BTN_SELECTOR);
+const prevButtons = getAllChild(sliderEl, PREV_BTN_SELECTOR);
+const nextButtons = getAllChild(sliderEl, NEXT_BTN_SELECTOR);
 
+const slides = [];
+const tabletSize = 1060;
+const mobileSize = 740;
+
+// eslint-disable-next-line no-use-before-define
+let cardCount = getCardCount();
 let currentIndex = 0;
+
+function getCardCount() {
+  const screenSize = window.innerWidth;
+  if (screenSize < mobileSize) {
+    return 1;
+  }
+  if (screenSize < tabletSize) {
+    return 2;
+  }
+  return 3;
+}
+
+const getSlides = () => {
+  const pets = getAllPets();
+
+  if (!Array.isArray(pets)) return;
+
+    for (let i = 0; i < pets.length; i += cardCount) {
+    const cardsSet = pets.slice(i, i + cardCount);
+
+    if (cardsSet.length !== cardCount) {
+      cardsSet.push(pets[0]);
+    }
+
+    const cards = cardsSet.reduce((acc, pet) => `${acc} ${getCard(pet)}`, '');
+    const slide = `<div class="slider__slide"><div class="friends__cards">${cards}</div></div>`;
+
+    slides.push(slide);
+  }
+
+  sliderWrapper.innerHTML = slides.join('');
+};
+
+const resizeHandler = () => {
+  const newCardCount = getCardCount();
+
+  if (cardCount !== newCardCount) {
+    cardCount = newCardCount;
+    slides.length = 0;
+    getSlides();
+  }
+};
 
 function showSlide(index) {
   const offset = -index * 100;
@@ -34,8 +80,11 @@ function showPrevSlide() {
 const slider = () => {
   if (!sliderEl) return;
 
-  nextButton.addEventListener('click', showNextSlide);
-  prevButton.addEventListener('click', showPrevSlide);
+  getSlides();
+  window.addEventListener('resize', resizeHandler);
+
+  nextButtons.forEach((btn) => btn.addEventListener('click', showNextSlide));
+  prevButtons.forEach((btn) => btn.addEventListener('click', showPrevSlide));
 };
 
 export default slider;
